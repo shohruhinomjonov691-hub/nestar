c7079eff9a2456d299759d2ab9e06809873ef8a0
shokhrukhbek <shohruhinomjonov691@gmail.com>
2026-04-01 17:31:22 +0900

    fix: connect MongoDB database

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -17,8 +17,8 @@ import { DatabaseModule } from './database/database.module';
 			uploads: false,
 			autoSchemaFile: true,
 		}),
-		ComponentsModule,
-		DatabaseModule,
+		ComponentsModule, // HTTP
+		DatabaseModule, // TCP
 	],
 	controllers: [AppController],
 	providers: [AppService, AppResolver],
