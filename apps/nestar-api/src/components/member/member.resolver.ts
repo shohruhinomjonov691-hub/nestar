42ec98c7e7db56d99ecee48988d9e9a976ffab10
shokhrukhbek <shohruhinomjonov691@gmail.com>
2026-04-14 12:41:59 +0900

    feat: develop getMember GraphQL api

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -10,6 +10,7 @@ import { RolesGuard } from '../auth/guards/roles.guard';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { MemberType } from '../../libs/enums/member.enum';
 import { MemberUpdate } from '../../libs/dto/member/member.update';
+import { shapeIntoMongoObjectId } from '../../libs/config';
 
 @Resolver()
 export class MemberResolver {
@@ -55,10 +56,11 @@ export class MemberResolver {
 		return this.memberService.updateMember(memberId, { _id: memberId, ...inputWithoutId });
 	}
 
-	@Query(() => String)
-	public async getMember(): Promise<String> {
+	@Query(() => Member)
+	public async getMember(@Args('memberId') input: string): Promise<Member> {
 		console.log('Query: getMember');
-		return this.memberService.getMember();
+		const targetId = shapeIntoMongoObjectId(input);
+		return this.memberService.getMember(targetId);
 	}
 
 	/** ADMIN **/
