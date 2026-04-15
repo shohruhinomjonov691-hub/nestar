e18037493857b26de76576a62733602043b3a0e0
shokhrukhbek <shohruhinomjonov691@gmail.com>
2026-04-15 15:08:38 +0900

    fix: modification of property module

---
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -0,0 +1,9 @@
+import { Resolver } from '@nestjs/graphql';
+import { PropertyService } from './property.service';
+
+@Resolver()
+export class PropertyResolver {
+    constructor(private readonly propertyService: PropertyService) {}
+
+      
+}
