3a04ac775240af0d8539501279f5581520c204bc
shokhrukhbek <shohruhinomjonov691@gmail.com>
2026-04-22 17:13:39 +0900

    feat: develop likeTargetBoardArticle GraphQL api

--- apps/nestar-api/src/components/board-article/board-article.module.ts
+++ apps/nestar-api/src/components/board-article/board-article.module.ts
@@ -6,6 +6,7 @@ import BoardArticleSchema from '../../schemas/BoardArticle.model';
 import { AuthModule } from '../auth/auth.module';
 import { ViewModule } from '../view/view.module';
 import { MemberModule } from '../member/member.module';
+import { LikeModule } from '../like/like.module';
 
 @Module({
 	imports: [
@@ -18,6 +19,7 @@ import { MemberModule } from '../member/member.module';
 		AuthModule,
 		MemberModule,
 		ViewModule,
+		LikeModule,
 	],
 	providers: [BoardArticleResolver, BoardArticleService],
 	exports: [BoardArticleService],
