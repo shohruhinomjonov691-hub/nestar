4cbb27a88a3c9cd4e96f15b0d303b40259c78725
shokhrukhbek <shohruhinomjonov691@gmail.com>
2026-04-22 16:30:20 +0900

    feat: develop likeTargetMember GraphQL api

--- apps/nestar-api/src/components/like/like.module.ts
+++ apps/nestar-api/src/components/like/like.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { LikeService } from './like.service';
 import LikeSchema from '../../schemas/Like.model';
+import { LikeInput } from '../../libs/dto/like/like.input';
 
 @Module({
 	imports: [
@@ -15,4 +16,8 @@ import LikeSchema from '../../schemas/Like.model';
 	providers: [LikeService],
 	exports: [LikeService],
 })
-export class LikeModule {}
+export class LikeModule {
+	toggleLike(input: LikeInput): number | PromiseLike<number> {
+		throw new Error('Method not implemented.');
+	}
+}
